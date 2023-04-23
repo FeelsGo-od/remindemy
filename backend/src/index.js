158d6d561446460549d91275d0041d1bbfd87099
nocomac <andrew.sokolowsky@gmail.com>
2023-04-23 17:45:29 +0200

    allow credentials in vercel.json 1.8

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,11 @@ const PORT = 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://remindemy-react.vercel.app"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://remindemy-react.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.get('/', (req, res) => {
 	res.send("<h2>Hello world!</h2>");
