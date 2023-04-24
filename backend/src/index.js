a730a2eb32d828144bdefa9a08ad57c4cb20b203
nocomac <andrew.sokolowsky@gmail.com>
2023-04-24 21:11:07 +0200

    add access control methods to backend 0.2

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
   if(allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
   next();
 });
