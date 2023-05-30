62b677e2829fe745d9ca7c549ed54d9d3a1e8ee6
nocomac <andrew.sokolowsky@gmail.com>
2023-05-30 12:12:10 +0200

    add scheduled email sender with croner

--- backend/src/email.js
+++ backend/src/email.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
-const schedule = require('node-schedule');
+const Cron = require("croner");
+const moment = require('moment');
 
 const sendEmailWithTopic = (data) => {
     let mailOptions = {
@@ -17,41 +18,32 @@ const sendEmailWithTopic = (data) => {
         }
     })
 
-    const sendEmail = transporter.sendMail(mailOptions, (error, info) => {
-        if(error) {
-            console.log(error)
-        } else {
-            console.log(`Email sent: ` + info.response)
-            // Set new Date and update count(to count times topic was already sent to user)
-        }
-    })
-
-    sendEmail
-      
-    function addDays(date, days) {
-        const dateCopy = new Date(date);
-        dateCopy.setDate(date.getDay() + days);
-    
-        return dateCopy;
+    const sendEmail = () => {
+        transporter.sendMail(mailOptions, (error, info) => {
+            if(error) {
+                console.log(error)
+            } else {
+                console.log(`Email sent: ` + info.response)
+                // Set new Date and update count(to count times topic was already sent to user)
+            }
+        })
     }
     
     const date = new Date();
 
-    const tomorrow = addDays(date, 1)
-    const fourthDay = addDays(date, 3)
-    const seventhDay = addDays(date, 6)
-    
-    const job = schedule.scheduleJob(tomorrow, () => {
-        sendEmail
-    })
-    
-    const job2 = schedule.scheduleJob(fourthDay, () => {
-        sendEmail
-    })
-    
-    const job3 = schedule.scheduleJob(seventhDay, () => {
-        sendEmail
-    })
+    // const tomorrow = addDays(date, 1)
+    // const fourthDay = addDays(date, 3)
+    // const seventhDay = addDays(date, 6)
+    // const tomorrow = addMinutes(date, 1)
+
+    const tomorrow = moment(date).add(1, 'm').toDate();
+
+    sendEmail()
+
+    Cron(tomorrow, () => {
+        console.log('Yay!') 
+        sendEmail()
+    });
 }
 
 module.exports = {
